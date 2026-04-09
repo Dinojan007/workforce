8383df083ad27b9d342cb460ec36ed3a44e7a13a
DInojan777 <DInojan777@gmail.com>
2026-04-09 17:25:59 +0530

    add postgersql and docker file

--- jobfinder/jobfinder/settings.py
+++ jobfinder/jobfinder/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'jobfinder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jobfinder_db',
+        'USER': 'myuser',      
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
